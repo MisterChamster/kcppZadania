bd411dc7822770682ce6e84d29dc286019b4ea5a
MisterChamster <s.duda03@gmail.com>
2025-06-11 15:56:51 +0200

    Update ZadKcpp class

--- ZadClassKcpp/src/ZadKcpp.cc
+++ ZadClassKcpp/src/ZadKcpp.cc
@@ -1,1 +1,41 @@
 #include "ZadKcpp.h"
+
+void ZadKcpp::Exercise1() {
+    
+}
+
+void ZadKcpp::Exercise2() {
+    
+}
+
+void ZadKcpp::Exercise3() {
+    
+}
+
+void ZadKcpp::Exercise4() {
+    
+}
+
+void ZadKcpp::Exercise5() {
+    
+}
+
+void ZadKcpp::Exercise6() {
+    
+}
+
+void ZadKcpp::Exercise7() {
+    
+}
+
+void ZadKcpp::Exercise8() {
+    
+}
+
+void ZadKcpp::Exercise9() {
+    
+}
+
+void ZadKcpp::Exercise10() {
+    
+}§
