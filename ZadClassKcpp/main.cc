6043d80ed2f21dcedd0b1e4d73c1a5c94b42b2fd
MisterChamster <s.duda03@gmail.com>
2025-06-12 23:56:41 +0200

    Exercise 3

--- ZadClassKcpp/main.cc
+++ ZadClassKcpp/main.cc
@@ -9,18 +9,20 @@
 //    - numer zadania w sekcji i opis w kilu słowach co dana funkcja (zadanie) zawiera
 #include "include/ZadKcpp.h"
 #include <iostream>
+// using namespace std;
+
 
 int main() {
     // ZadKcpp::Exercise1();
-    ZadKcpp::Exercise2();
+    // ZadKcpp::Exercise2();
     // ZadKcpp::Exercise3();
-    // ZadKcpp::Exercise4();
-    // ZadKcpp::Exercise5();
-    // ZadKcpp::Exercise6();
-    // ZadKcpp::Exercise7();
-    // ZadKcpp::Exercise8();
-    // ZadKcpp::Exercise9();
-    // ZadKcpp::Exercise10();
+    ZadKcpp::Exercise4();
+    ZadKcpp::Exercise5();
+    ZadKcpp::Exercise6();
+    ZadKcpp::Exercise7();
+    ZadKcpp::Exercise8();
+    ZadKcpp::Exercise9();
+    ZadKcpp::Exercise10();
 
     return 0;
 }
