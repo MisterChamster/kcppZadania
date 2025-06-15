af7429f688f093c06f901492a755ad14bf56fab5
Mr Chamster <s.duda03@gmail.com>
2025-06-15 02:32:41 +0200

    Finish

--- ZadClassKcpp/main.cc
+++ ZadClassKcpp/main.cc
@@ -1,23 +1,74 @@
-// - Program powinien zawierać interaktywne menu z możliwością wyboru zadania do wywołania 
-//   - menu:
-//    - sekcja
-//    - numer zadania w sekcji i opis w kilu słowach co dana funkcja (zadanie) zawiera
 #include "include/ZadKcpp.h"
 #include <iostream>
-// using namespace std;
+using namespace std;
 
 
 int main() {
-    // ZadKcpp::Exercise1();
-    // ZadKcpp::Exercise2();
-    // ZadKcpp::Exercise3();
-    // ZadKcpp::Exercise4();
-    // ZadKcpp::Exercise5();
-    // ZadKcpp::Exercise6();
-    // ZadKcpp::Exercise7();
-    // ZadKcpp::Exercise8();
-    // ZadKcpp::Exercise9();
-    // ZadKcpp::Exercise10();
+    string input;
+    cout<<endl;
+
+    while(true){
+        cout<<"================= Welcome! ================="<<endl;
+        cout<<"1 - ZadEnumExample (1.0)"<<endl;
+        cout<<"2 - ZadClassExample (1.1)"<<endl;
+        cout<<"3 - ZadStringExample (1.2)"<<endl;
+        cout<<"4 - ZadZwracanie (2.1)"<<endl;
+        cout<<"5 - ZadPrzeciazaniePole (2.2)"<<endl;
+        cout<<"6 - ZadPrzeciazanieDodaj (2.3)"<<endl;
+        cout<<"7 - ZadPrzeciazaniePlus (2.4)"<<endl;
+        cout<<"8 - LPrzeciazaniePotega (2.5)"<<endl;
+        cout<<"9 - ZadArytmetykaWskaznikowZamiana (3.1)"<<endl;
+        cout<<"10 - ZadMetodyAbstrakcyjne (5.6)"<<endl;
+        cout<<"exit - user ends program\n\n>> ";
+        cin>>input;
+        cout<<endl;
+
+        if(input=="1"){
+            ZadKcpp::Exercise1();
+        }
+
+        else if(input=="2"){
+            ZadKcpp::Exercise2();
+        }
+
+        else if(input=="3"){
+            ZadKcpp::Exercise3();
+        }
+
+        else if(input=="4"){
+            ZadKcpp::Exercise4();
+        }
+
+        else if(input=="5"){
+            ZadKcpp::Exercise5();
+        }
+
+        else if(input=="6"){
+            ZadKcpp::Exercise6();
+        }
+
+        else if(input=="7"){
+            ZadKcpp::Exercise7();
+        }
+
+        else if(input=="8"){
+            ZadKcpp::Exercise8();
+        }
+
+        else if(input=="9"){
+            ZadKcpp::Exercise9();
+        }
+
+        else if(input=="10"){
+            ZadKcpp::Exercise10();
+        }
+
+        else if(input=="exit"){
+            break;
+        }
+
+        cout<<endl<<endl<<endl;
+    }
 
     return 0;
 }
--- ZadClassKcpp/src/Zad3Kcpp.cc
+++ ZadClassKcpp/src/Zad3Kcpp.cc
@@ -11,8 +11,8 @@ void Zad3Kcpp::runTask() {
     if(fridge.empty()) cout<<"Your fridge is empty. You must be hungry.\n";
 
     string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-    cout<<"Długość stringa jest równa: "<<lorem.size()<<endl;
-    cout<<"Długość stringa jest równa: "<<lorem.length()<<endl;
+    cout<<"Dlugosc stringa jest rowna: "<<lorem.size()<<endl;
+    cout<<"Dlugosc stringa jest rowna: "<<lorem.length()<<endl;
 
     string name = "Katarzyna";
     cout<<"Znak na miejscu o indeksie 3. to: "<<name.at(3)<<endl;
@@ -33,18 +33,18 @@ void Zad3Kcpp::runTask() {
     string language1 = "Bonjour!";
     string language2 = "Guten Tag!";
 
-    cout << "Pierwszy język: " << language1 << endl;
-    cout << "Drugi język: " << language2 << endl;
+    cout << "Pierwszy jezyk: " << language1 << endl;
+    cout << "Drugi jezyk: " << language2 << endl;
     language1.swap(language2);
-    cout << "Pierwszy język: " << language1 << endl;
-    cout << "Drugi język: " << language2 << endl;
+    cout << "Pierwszy jezyk: " << language1 << endl;
+    cout << "Drugi jezyk: " << language2 << endl;
     
     string dream = "Pragne kiedys pracowac w CERN, jest to moje marzenie.";
     string sub_string = dream.substr(25, 4);
     cout << "Substring starting from index 25 with length of 4 characters: " << sub_string << endl;
 
-    string okej = "lubię ";
+    string okej = "lubie ";
     okej.append("Cpp?");
-    cout<<"Czy wiedział Pan że "<<okej<<endl;
+    cout<<"Czy wiedzial Pan ze "<<okej<<endl;
 
 }
